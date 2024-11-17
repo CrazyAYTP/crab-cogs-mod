b9de9b9a757b3dca7f8b6b93159498577d70c14c
CrazyAYTP <67873645+CrazyAYTP@users.noreply.github.com>
2024-11-17 20:34:17 +0100

    Update __init__.py

--- gelbooru/__init__.py
+++ gelbooru/__init__.py
@@ -1,7 +1,7 @@
-from .gelbooru import Booru
+from .rule34 import Rule34
 from redbot.core.utils import get_end_user_data_statement
 
 __red_end_user_data_statement__ = get_end_user_data_statement(__file__)
 
 async def setup(bot):
-    await bot.add_cog(Booru(bot))
+    await bot.add_cog(Rule34(bot))
